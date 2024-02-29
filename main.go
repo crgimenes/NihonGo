84ebe0aa9244a42b5d95bdf9ab54ce833a2dc6c5
crg <crg>
2024-02-29 01:21:23 +0000

    add sqlite

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "modernc.org/sqlite"
+)
+
+const (
+	connectionString = `file:nihongo.db?mode=rwc&_journal_mode=WAL&_busy_timeout=10000`
+)
+
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	db, err := sqlx.Open("sqlite", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
 }
