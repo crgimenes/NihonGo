4e051ca85074280351c018dbab9b6b5fca0d93da
crg <crg>
2024-03-05 09:40:55 +0000

    add readline

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/jmoiron/sqlx v1.3.5
+	golang.org/x/term v0.18.0
 	modernc.org/sqlite v1.29.2
 )
 
@@ -14,8 +15,8 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	modernc.org/gc/v3 v3.0.0-20240304020402-f0dba7c97c2b // indirect
 	modernc.org/libc v1.41.0 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
 	modernc.org/memory v1.7.2 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jmoiron/sqlx"
+	"golang.org/x/term"
 	_ "modernc.org/sqlite"
 )
 
 const (
 	connectionString = `file:nihongo.db?mode=rwc&_journal_mode=WAL&_busy_timeout=10000`
 )
+
+func readLine() (string, error) {
+	if !term.IsTerminal(int(os.Stdin.Fd())) {
+		return "", fmt.Errorf("pipe not supported")
+	}
+
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		return "", fmt.Errorf("failed setting stdin to raw mode: %w", err)
+	}
+	tty := term.NewTerminal(os.Stdin, "")
+	line, err := tty.ReadLine()
+	_ = term.Restore(int(os.Stdin.Fd()), oldState)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to read from stdin: %w", err)
+	}
+	return line, nil
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -20,5 +42,12 @@ func main() {
 	}
 
 	defer db.Close()
+
+	fmt.Printf("> ")
+	line, err := readLine()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%q\n", line)
 
 }
